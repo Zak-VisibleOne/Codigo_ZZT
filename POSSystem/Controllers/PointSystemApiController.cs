ce00efa074ac219c95e631ddacda1f74c120e161
Zak Visibee <zak@visibleone.com>
2023-02-15 14:38:46 +0630

    update point api

--- POSSystem/Controllers/PointSystemApiController.cs
+++ POSSystem/Controllers/PointSystemApiController.cs
@@ -8,6 +8,9 @@ using System.Data;
 using System.Web;
 using System.Collections.Generic;
 using POSSystem.Utils;
+using System.Security.Authentication;
+using System.IO;
+using Newtonsoft.Json;
 //using System.Web.Http;
 //using System.Net.Http.Headers;
 namespace POSSystem.Controllers
@@ -15,15 +18,9 @@ namespace POSSystem.Controllers
     public class PointSystemApiController : ApiController
     {
         private POSSystemEntities Context { get; set; }
-        //string apiurl = "";
         public PointSystemApiController()
         {
             Context = new POSSystemEntities();
-            //var siteInfo = Context.SiteInformations.FirstOrDefault();
-            //if (siteInfo != null)
-            //{
-            //    apiurl = siteInfo.SiteUrl;
-            //}
         }
         public class ReturnMsg
         {
@@ -31,11 +28,57 @@ namespace POSSystem.Controllers
             public string eflag { set; get; }
         }
 
-        //Calculate Point
-
+        public class ViewModelPurchase
+        {
+            public int id { set; get; }
+            public string MemberCode { get; set; }
+            public string CouponCode { get; set; }
+            public string ReceiptNumber { get; set; }
+            public string ItemName { get; set; }
+            public double Price { get; set; }
+            public int Qty { get; set; }
+            public double TotalPrice { get; set; }
+        }
+        
+        [HttpPost]
+        public HttpResponseMessage CalculatePoint()
+        {
+            ReturnMsg msg = new ReturnMsg();
+            const SslProtocols _Tls12 = (SslProtocols)0x00000C00;
+            const SecurityProtocolType Tls12 = (SecurityProtocolType)_Tls12;
+            ServicePointManager.SecurityProtocol = Tls12;
+            var httpWebRequest = System.Net.WebRequest.Create(@"https://localhost:44361/api/possystemapi/getpurchasedata") as HttpWebRequest;
+            httpWebRequest.ContentType = "application /json;";
+            httpWebRequest.Method = "GET";
+            httpWebRequest.Timeout = 100000;
+            var httpResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+            using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
+            {
+                var result = streamReader.ReadToEnd();
+                var purchaseLst = JsonConvert.DeserializeObject<List<ViewModelPurchase>>(result);
+                if (purchaseLst.Count > 0)
+                {
+                    foreach (var item in purchaseLst)
+                    {
+                        if (item.ItemName == "Non Alcohol")
+                        {
+                            int point = Convert.ToInt32(item.TotalPrice / 10);
+                            var memberObj = Context.Members.Where(x => x.MemberCode == item.MemberCode).FirstOrDefault();
+                            if (memberObj != null)
+                            {
+                                memberObj.TotalPoint = memberObj.TotalPoint + point;
+                                Context.SaveChanges();
+                                msg.msg = "Success";
+                                msg.eflag = "S";
+                            }
+                        }
+                    }
+                }
+            }
+            return Request.CreateResponse(HttpStatusCode.OK, msg);
+        }
 
         [HttpPost]
-        //[Route("api/pointsystemapi/addpointtomemer")]
         public HttpResponseMessage AddPointToMember()
         {
             var code = HttpContext.Current.Request.Params["membercode"];
@@ -69,7 +112,6 @@ namespace POSSystem.Controllers
                 throw;
             }
         }
-
 
     }
 }
