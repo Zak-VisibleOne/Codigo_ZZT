edd6cb4b586fb2cbc229a1154523332329e6c62a
Zak Visibee <zak@visibleone.com>
2023-02-15 16:49:02 +0630

    update coupon

---
+++ POSSystem/Controllers/CMSApiController.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using POSData;
+using System.Data;
+using System.Web;
+using System.Collections.Generic;
+using POSSystem.Utils;
+using System.Security.Authentication;
+using System.IO;
+using Newtonsoft.Json;
+//using System.Web.Http;
+//using System.Net.Http.Headers;
+namespace POSSystem.Controllers
+{
+    public class CMSApiController : ApiController
+    {
+        private POSSystemEntities Context { get; set; }
+        public CMSApiController()
+        {
+            Context = new POSSystemEntities();
+        }
+        public class ReturnMsg
+        {
+            public string msg { set; get; }
+            public string eflag { set; get; }
+        }
+
+        public class ViewModelPurchase
+        {
+            public int id { set; get; }
+            public string MemberCode { get; set; }
+            public string CouponCode { get; set; }
+            public string ReceiptNumber { get; set; }
+            public string ItemName { get; set; }
+            public double Price { get; set; }
+            public int Qty { get; set; }
+            public double TotalPrice { get; set; }
+        }
+        
+        [HttpPost]
+        public HttpResponseMessage CalculatePoint()
+        {
+            ReturnMsg msg = new ReturnMsg();
+            const SslProtocols _Tls12 = (SslProtocols)0x00000C00;
+            const SecurityProtocolType Tls12 = (SecurityProtocolType)_Tls12;
+            ServicePointManager.SecurityProtocol = Tls12;
+            var httpWebRequest = System.Net.WebRequest.Create(@"https://localhost:44361/api/possystemapi/getpurchasedata") as HttpWebRequest;
+            httpWebRequest.ContentType = "application /json;";
+            httpWebRequest.Method = "GET";
+            httpWebRequest.Timeout = 100000;
+            var httpResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+            using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
+            {
+                var result = streamReader.ReadToEnd();
+                var purchaseLst = JsonConvert.DeserializeObject<List<ViewModelPurchase>>(result);
+                if (purchaseLst.Count > 0)
+                {
+                    foreach (var item in purchaseLst)
+                    {
+                        if (item.ItemName == "Non Alcohol")
+                        {
+                            int point = Convert.ToInt32(item.TotalPrice / 10);
+                            var memberObj = Context.Members.Where(x => x.MemberCode == item.MemberCode).FirstOrDefault();
+                            if (memberObj != null)
+                            {
+                                memberObj.TotalPoint = memberObj.TotalPoint + point;
+                                Context.SaveChanges();
+                                msg.msg = "Success";
+                                msg.eflag = "S";
+                            }
+                        }
+                    }
+                }
+            }
+            return Request.CreateResponse(HttpStatusCode.OK, msg);
+        }
+
+        [HttpPost]
+        public HttpResponseMessage AddPointToMember()
+        {
+            var code = HttpContext.Current.Request.Params["membercode"];
+            var point = HttpContext.Current.Request.Params["point"];
+            ReturnMsg msg = new ReturnMsg();
+            try
+            {
+                using (var context = new POSSystemEntities())
+                {
+                    var mObj = context.Members.Where(x => x.MemberCode == code).FirstOrDefault();
+                    if (mObj != null)
+                    {
+                        mObj.TotalPoint = 100;
+                        context.SaveChanges();
+                        msg.msg = "Success";
+                        msg.eflag = "S";
+                    }
+                    else
+                    {
+                        msg.msg = "Member not found.";
+                        msg.eflag = "E";
+                    }
+                    return Request.CreateResponse(HttpStatusCode.OK, msg);
+                }
+            }
+            catch (Exception e)
+            {
+                msg.msg = e.Message;
+                msg.eflag = "E";
+                return Request.CreateResponse(HttpStatusCode.BadRequest, msg);
+                throw;
+            }
+        }
+
+    }
+}
---
+++ POSSystem/Controllers/CouponController.cs
@@ -0,0 +1,138 @@
+﻿using POSData;
+using POSSystem.Models;
+using System.Linq;
+using System.Web.Mvc;
+using System.Collections.Generic;
+//using System.Data.Entity.Validation;
+using System;
+using System.Data;
+
+namespace POSSystem.Controllers
+{
+    public class CouponController : Controller
+    {
+        log4net.ILog logger = log4net.LogManager.GetLogger(typeof(DepartmentController));
+        private POSSystemEntities Context { get; set; }
+        //Index page
+        public ActionResult Index()
+        {
+            var User = System.Web.HttpContext.Current.Session["User"];
+            if (User == null)
+            {
+                return RedirectToAction("../Login");
+            }
+            return View();
+        }
+        public CouponController()
+        {
+            Context = new POSSystemEntities();
+        }
+        public class ViewModelCoupon
+        {
+            public int ID { get; set; }
+            public string CouponCode { get; set; }
+            public string CouponName { get; set; }
+            public string QRCodeImageUrl { get; set; }
+            public Nullable<int> AvailableQty { get; set; }
+            public string StartDate { get; set; }
+            public string EndDate { get; set; }
+            public Nullable<double> DiscountAmount { get; set; }
+            public string Method { get; set; }
+        }
+        public JsonResult GetCouponList()
+        {
+            var data = new List<object[]>();
+            var list = Context.Coupons.ToList();
+            var list_count = list.Count;
+            if (list.Count > 0)
+            {
+                foreach (Coupon l in list)
+                {
+                    data.Add(new object[] {
+                        l.CouponCode
+                       ,l.CouponName
+                       ,l.AvailableQty
+                       ,Convert.ToDateTime(l.StartDate).ToString("dd/MM/yyyy")
+                       ,Convert.ToDateTime(l.EndDate).ToString("dd/MM/yyyy")
+                       ,l.DiscountAmount
+                       , ""
+                    });
+                }
+            }
+            return Json(
+                    new
+                    {
+                        //draw = draw,
+                        recordsTotal = list_count,
+                        recordsFiltered = list_count,
+                        data = data
+                    }, JsonRequestBehavior.AllowGet
+            );
+        }
+        public JsonResult GetCouponByID(int ID)
+        {
+            var data = Context.Coupons.Where(x => x.ID == ID).FirstOrDefault();
+            return Json(new { data }, JsonRequestBehavior.AllowGet);
+        }
+        public JsonResult DeleteCoupontByID(int ID)
+        {
+            ResultMessage result = new ResultMessage();
+            using (var context = new POSSystemEntities())
+            {
+                context.Database.ExecuteSqlCommand(@"Delete Coupon where ID = {0}", new object[] { ID });
+                context.SaveChanges();
+                result.message = "Data successfully deleted";
+                result.result = "success";
+            }
+            return Json(new { result }, JsonRequestBehavior.AllowGet);
+        }
+        public JsonResult SaveUpdateDepartment(ViewModelCoupon m)
+        {
+            ResultMessage result = new ResultMessage();
+            if (m.Method == "add")
+            {
+                using (var context = new POSSystemEntities())
+                {
+                    Coupon coupon = new Coupon();
+                    coupon.CouponCode = m.CouponCode ?? "";
+                    coupon.CouponName = m.CouponName ?? "";
+                    coupon.QRCodeImageUrl = "/";
+                    coupon.AvailableQty = m.AvailableQty;
+                    coupon.StartDate = Convert.ToDateTime(m.StartDate);
+                    coupon.EndDate = Convert.ToDateTime(m.EndDate);
+                    coupon.DiscountAmount = m.DiscountAmount;
+                    context.Coupons.Add(coupon);
+                    context.SaveChanges();
+                }
+                result.message = "New coupon data save successfully.";
+                result.result = "success";
+            }
+            else
+            {
+                using (var context = new POSSystemEntities())
+                {
+                    var coupon = context.Coupons.Where(x => x.ID == m.ID).FirstOrDefault();
+                    if (coupon != null)
+                    {
+                        coupon.CouponCode = m.CouponCode ?? "";
+                        coupon.CouponName = m.CouponName ?? "";
+                        coupon.QRCodeImageUrl = "/";
+                        coupon.AvailableQty = m.AvailableQty;
+                        coupon.StartDate = Convert.ToDateTime(m.StartDate);
+                        coupon.EndDate = Convert.ToDateTime(m.EndDate);
+                        coupon.DiscountAmount = m.DiscountAmount;
+                        context.SaveChanges();
+                        result.message = "Coupon has been updated successfully.";
+                        result.result = "success";
+                    }
+                    else
+                    {
+                        result.message = "There is no data to update.";
+                        result.result = "info";
+                    }
+                }
+            }
+            return Json(new { result }, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
